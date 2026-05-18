a32dd1ee2e5b1e0ada4659491bc3e9382c4cf67e
Saidul Islam <mdsaidulislam.dev@gmail.com>
2026-05-18 23:22:03 +0600

    Configure the mysqlclient database

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# For development, you can use SQLite. For production, switch to MySQL or PostgreSQL.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
